387f0f70d5234e8bd440538cdc63f7651e2b7d98
MarionNafula <wekesamarion100@gmail.com>
2023-04-21 18:40:16 +0300

    Completed assignment

--- src/parks/RockyMountain.js
+++ src/parks/RockyMountain.js
@@ -1,11 +1,16 @@
+export const trees = "Aspen and Pine";
 const trees = "Aspen and Pine";
 
+export function wildlife() 
 function wildlife() {
   console.log("Elk, Bighorn Sheep, Moose");
 }
 
-function elevation() {
+function elevation() 
+function Elevation() {
   console.log("9583 ft");
+
 }
 
 export { trees, wildlife };
+
